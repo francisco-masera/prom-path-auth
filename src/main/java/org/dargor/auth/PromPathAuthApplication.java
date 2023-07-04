212e6f7de8e144b3e83727bcf5dd2acece457199
Dargor94 <franciscomasera1994@gmail.com>
2023-07-04 02:14:47 -0300

    init

---
+++ src/main/java/org/dargor/auth/PromPathAuthApplication.java
@@ -0,0 +1,13 @@
+package org.dargor.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromPathAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromPathAuthApplication.class, args);
+    }
+
+}
