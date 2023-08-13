dab63d46cff4e959600e89e3a2d9099edee68b45
Dargor94 <franciscomasera1994@gmail.com>
2023-08-13 01:16:02 -0300

    jaeger

--- src/main/java/org/dargor/auth/config/TracingConfig.java
+++ src/main/java/org/dargor/auth/config/TracingConfig.java
@@ -1,8 +1,10 @@
 package org.dargor.auth.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnProperty(value = "opentracing.jaeger.enabled", havingValue = "false")
 @Configuration
 public class TracingConfig {
 
